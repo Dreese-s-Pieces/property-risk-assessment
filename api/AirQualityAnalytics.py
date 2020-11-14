508c33f15183ca3981636ebc5a88e3daf51e9399
iRG9 <romikghosh1@gmail.com>
2020-11-14 15:54:39 -0500

    Added elementary air quality analytics

---
+++ api/AirQualityAnalytics.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import requests
+
+def make_dataframe(r):
+    """Extracts data from request r and returns a DataFrame."""
+    rows = []
+    for item in r.json()['data']:
+        rows.append([item['lat'], item['lon'], item['aqi'], item['station']['name']])
+    df = pd.DataFrame(rows, columns=['lat', 'lon', 'aqi', 'name'])
+    df['aqi'] = pd.to_numeric(df.aqi, errors='coerce')
+    return df
+
+
+def get_request_data(url):
+    return requests.get(url).json()
+
+
+
+class AirQualityAnalytics():
+    def __init__(self):
+        self.city_str = ""
+        self.url = "https://api.waqi.info/feed/" + self.city_str + "/?token=fe269bc83b983ff958090f5808afa12eed57f14f"
+
+    def one_samp_t_test(self, df, city_str):
+        return (df[df['name'].split(",")[0] == city_str]['aqi']-df['aqi'].mean())/(df['aqi'].var()**(1/2)/ df.count()**(1/2))
+
+    def get_local_air_quality_comparison(self, city_str):
+        self.city_str = city_str
+        req_data = self.get_request_data(self.url)
+        local_df = make_dataframe(req_data)
+        air_quality_comp = dict()
+        air_quality_comp['Deviation of AQI From Closest Cities'] = local_df[local_df['name'].split(',')[0] == city_str]['aqi'].sum()-local_df['aqi'].sum()/local_df.count()
+        air_quality_comp['Probability of AQI Being Significantly Different From Surrounding Cities'] = self.one_samp_t_test(local_df, self.city_str)
+
+        return air_quality_comp
+
+
+
+    def get_air_quality_index(self, city_str):
+        self.city_str = city_str
+        return self.get_request_data(self.url)["data"]["aqi"]
--- api/FEMA_House_Dmg_Analytics.py
+++ api/FEMA_House_Dmg_Analytics.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import requests
 
+
 class Disaster_Stats():
-    def __init__(self, url = "https://www.fema.gov/api/open/v2/HousingAssistanceOwners"):
+    def __init__(self, url="https://www.fema.gov/api/open/v2/HousingAssistanceOwners"):
         self.disasters_csv = requests.get(url).json()
         self.disasters_csv = pd.DataFrame.from_dict(self.disasters_csv["HousingAssistanceOwners"])
 
@@ -13,7 +14,7 @@ class Disaster_Stats():
         state_csv = self.disasters_csv.get_state_level_get_state_level_disasters(state_str)
         state_csv = state_csv[state_csv['declarationTitle'] == disaster]
         nat_csv = self.disasters_csv[self.disasters_csv['declarationTitle'] == disaster]
-        return state_csv.count()/nat_csv.count()
+        return state_csv.count() / nat_csv.count()
 
     def get_total_dmg_for_zip(self, zip_str):
         return self.disasters_csv[self.disasters_csv['zipCode'] == zip_str]['averageFemaInspectedDamage'].sum()
@@ -24,8 +25,8 @@ class Disaster_Stats():
     def get_total_dmg(self):
         return self.disasters_csv['averageFemaInspectedDamage'].sum()
 
-    def get_prop_zip_dmg_for_state(self, zip_str,state_str):
-        return self.disasters_csv.get_total_dmg_for_zip(zip_str)/self.disasters_csv.get_total_dmg_for_state(state_str)
+    def get_prop_zip_dmg_for_state(self, zip_str, state_str):
+        return self.disasters_csv.get_total_dmg_for_zip(zip_str) / self.disasters_csv.get_total_dmg_for_state(state_str)
 
     def get_prop_zip_dmg_for_nation(self, zip_str):
-        return self.disasters_csv.get_total_dmg_for_zip(zip_str)/self.disasters_csv.get_total_dmg()
+        return self.disasters_csv.get_total_dmg_for_zip(zip_str) / self.disasters_csv.get_total_dmg()
