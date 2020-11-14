c16eb60f7b3cb4262f1cdb254bc5c3ca2844474d
tamtzehei <18tam.tzehei@gmail.com>
2020-11-13 23:44:20 -0500

    Added test flask server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 export default App;
 
 function App() {
+  // const [currentTime, setCurrentTime] = useState(0);
+
+  // useEffect(() => {
+  //   fetch('/time').then(res => res.json()).then(data => {
+  //     console.log(data);
+  //     setCurrentTime(data.time);
+  //   });
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         Property Risk Assessment
       </header>
+      {/* <p>{currentTime}</p> */}
 
       <AddressForm/>
     </div>
